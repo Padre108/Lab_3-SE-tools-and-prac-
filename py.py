ebbe867c569e26837c1fdcfc5fa9f9241ba98867
Padre108 <nonoyjpgerona@gmail.com>
2024-09-18 16:19:19 +0800

    fixed bug for py.py

--- py.py
+++ py.py
@@ -1,18 +1,7 @@
 name = "John Patrick jr."
-age = 12.313
+age = 12 #changed this to a int
 
 
-# Bug
-#          *     *
-#           *   *
-#          ******* 
-#         *********   
-#        ***********
-#       *************
-#        ***********
-#         *********
-#           *    *
-#          **    **
 
 def addPerson(name: str, age: int):
     return {"name": name, "age": age}
