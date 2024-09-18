baeafd2015ebe58677a7aaf62cf420b7aefd2375
Padre108 <nonoyjpgerona@gmail.com>
2024-09-18 16:14:58 +0800

    there is a bug in main

---
+++ py.py
@@ -0,0 +1,19 @@
+name = "John Patrick jr."
+age = 12.313
+
+
+# Bug
+#          *     *
+#           *   *
+#          ******* 
+#         *********   
+#        ***********
+#       *************
+#        ***********
+#         *********
+#           *    *
+#          **    **
+
+def addPerson(name: str, age: int):
+    return {"name": name, "age": age}
+
